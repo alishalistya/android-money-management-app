59b793e5e5370c6910016e7ce75776efbb581321
vanessrw <vanessarebecca29@gmail.com>
2024-03-18 13:02:01 +0700

    add random transaction n bool

--- app/src/main/java/com/example/tubespbd/database/TransactionRepository.kt
+++ app/src/main/java/com/example/tubespbd/database/TransactionRepository.kt
@@ -1,6 +1,8 @@
 package com.example.tubespbd.database
 
 import androidx.lifecycle.LiveData
+import java.util.Date
+import kotlin.random.Random
 
 class TransactionRepository(private val transactionDao: TransactionDao) {
 
@@ -36,5 +38,36 @@ class TransactionRepository(private val transactionDao: TransactionDao) {
         return transactionDao.getTransactionsSumByCategory()
     }
 
-
+    // buat random transaction + bool func
+    fun generateAndInsertRandomTransaction(): Boolean {
+        val randomTransaction = generateRandomTransaction()
+        val transactionId = transactionDao.insertTransaction(randomTransaction)
+        return isTransactionInsertedSuccessfully(transactionId)
+    }
+    fun generateRandomTransaction(): Transaction {
+        val randomTitle = generateRandomTitle()
+        val categories = listOf("Pengeluaran", "Pemasukan")
+        val randomCategory = categories.random()
+        val randomAmount = Random.nextFloat() * 1000f
+        val randomLocation = generateRandomLocation()
+        val currentDate = Date().toString()
+        return Transaction(
+            category = randomCategory,
+            amount = randomAmount,
+            tanggal = currentDate,
+            title = randomTitle,
+            location = randomLocation
+        )
+    }
+    fun generateRandomTitle(): String {
+        val randomSuffix = String.format("%03d", Random.nextInt(1000))
+        return "title$randomSuffix"
+    }
+    fun generateRandomLocation(): String {
+        // Generate a random location string (for example, "Random Location 123")
+        return "Random Location ${Random.nextInt(1000)}"
+    }
+    fun isTransactionInsertedSuccessfully(transactionId: Long): Boolean {
+        return transactionId > 0
+    }
 }
